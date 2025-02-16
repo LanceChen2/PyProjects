bd74fce4f35eaabe808b9c9a27351ec897609adb
LanceChen2 <jchenlance@gmail.com>
2025-02-17 00:00:48 +0800

    minor adjustment

--- unsorted_list_generation/check_unsorted_list_distinct.py
+++ unsorted_list_generation/check_unsorted_list_distinct.py
@@ -4,7 +4,7 @@ NUM = 1000  #change number for different file(s)
 with open(f"C:\\Users\\user\\Python Projects\\unsorted_list_generation\\unsorted_list_of_{NUM}.txt") as file: 
     lines = [line.rstrip() for line in file]
 
-# check if there is any duplicates for comparison
+# check if there is any duplicates via set function for comparison
 print(lines)
 print(len(lines))
 lines = list(set(lines))
