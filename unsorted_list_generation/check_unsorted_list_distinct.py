3835e89c5ab8dc5377de0f0cb13784016cdf63d4
LanceChen2 <jchenlance@gmail.com>
2025-02-19 00:20:42 +0800

    20250219 updates

--- unsorted_list_generation/check_unsorted_list_distinct.py
+++ unsorted_list_generation/check_unsorted_list_distinct.py
@@ -1,9 +1,11 @@
 
-# read elements in txt file into a list
+# read elements from txt file into a list
 NUM = 1000  #change number for different file(s)
 with open(f"C:\\Users\\user\\Python Projects\\unsorted_list_generation\\unsorted_list_of_{NUM}.txt") as file: 
     lines = [line.rstrip() for line in file]
 
+
+# to write a function with 2 different variables for comparison
 # check if there is any duplicates via set function for comparison
 print(lines)
 print(len(lines))
