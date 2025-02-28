a50db87920fa13b27b18c8754a8d698f3313f7d9
LanceChen2 <jchenlance@gmail.com>
2025-02-28 21:34:15 +0800

    python scripting ongoing

--- Python-scripting/get_game_data.py
+++ Python-scripting/get_game_data.py
@@ -30,9 +30,17 @@ def get_name_from_paths(paths, to_strip):
 
     return new_names
 
+
 def create_dir(path): 
     if not os.path.exists(path): 
         os.mkdir(path)
+
+
+def copy_and_overwrite(source, dest): 
+    if os.path.exists(dest): 
+        shutil.rmtree(dest)
+    shutil.copytree(source, dest)
+
 
 def main(source, target): 
     cwd = os.getcwd()
@@ -40,9 +48,13 @@ def main(source, target):
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    new_game_dirs = get_name_from_paths(game_paths, "game")
+    new_game_dirs = get_name_from_paths(game_paths, "_game")
 
     create_dir(target_path)
+
+    for src, dest in zip(game_paths, new_game_dirs): 
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
 
 
 if __name__ == "__main__": 
